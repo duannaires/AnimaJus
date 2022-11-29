43502a6688b0c02bd865a76d7c9f3134bbca863b
douglassilvadeveloper <douglassilvadeveloper@gmail.com>
2022-11-28 23:46:24 -0300

    feat: add section-hero

---
+++ src/app/presentation/components/shared/section-hero/index.tsx
@@ -0,0 +1,15 @@
+export function SectionHero() {
+
+  return (
+    <div className="pt-4 pb-0 card-grid">
+      <div className="container">
+        <div className="row g-4">
+          <div className="col-lg-6">
+          
+          </div>
+          <div className="col-lg-6"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
