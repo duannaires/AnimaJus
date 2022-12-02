e641c7ae0486bf13356f01f28f65bbbea7e3f73b
douglassilvadeveloper <douglassilvadeveloper@gmail.com>
2022-12-01 23:30:44 -0300

    refactor: ensure props in articles

--- src/app/presentation/components/shared/article/index.tsx
+++ src/app/presentation/components/shared/article/index.tsx
@@ -1,11 +1,14 @@
-import imageArticle from "../../../../../assets/images/rights1.jpg"
+interface ArticleProps {
+  title: string
+  image: string
+}
 
-export function Article() {
+export function Article({ title, image }: ArticleProps) {
   return (
-      <div className="container">
+      <div className="container mb-4">
         <div className="article d-flex flex-md-row flex-column align-items-center shadow-sm aj-bg-light">
           <div className="article-image">
-            <img src={imageArticle} alt="Imagem de artigo" />
+            <img src={image} alt="Imagem de artigo" />
           </div>
           <div className="article-info p-3">
             <div className="article-info__publication mb-2 text-muted small">
@@ -16,7 +19,7 @@ export function Article() {
                   30 de novembro de 2022
               </time>
             </div>
-            <h2 className="article-info__title h4 h3-md h4-lg">Direito, Estado e Sociedade</h2>
+            <h2 className="article-info__title h4 h3-md h4-lg">{title}</h2>
             <p className="article-info__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ipsum nunc, laoreet eget rhoncus vitae, interdum in eros. Duis fringilla elit massa, nec dictum ligula commodo a. </p>
           </div>
         </div>
--- src/app/presentation/pages/home/index.tsx
+++ src/app/presentation/pages/home/index.tsx
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom'
 import imageAnimaJus from '../../../../assets/anima-jus-1.png'
 import { Article } from '../../components/shared/article'
 import { SectionHero } from '../../components/shared/section-hero'
+import imageArticle1 from "../../../../assets/images/rights1.jpg"
+import imageArticle2 from "../../../../assets/images/rights2.jpg"
 
 export function Home() {
   return (
     <Fragment>
       <SectionHero />
-      <Article />
+      <Article image={imageArticle1} title='Direito, Estado e Sociedade'/>
+      <Article image={imageArticle2} title='A luta pela igualdade em um país historicamente desigual'/>
     </Fragment>
   )
 }
