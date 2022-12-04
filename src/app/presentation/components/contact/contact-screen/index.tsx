a5a28ff185aba04a78a4a8627dd5ef6341692cdd
douglassilvadeveloper <douglassilvadeveloper@gmail.com>
2022-12-04 19:05:45 -0300

    feat: contact page

--- src/app/presentation/components/contact/contact-screen/index.tsx
+++ src/app/presentation/components/contact/contact-screen/index.tsx
@@ -4,8 +4,29 @@ interface ContactScreenProps {
 
 export function ContactScreen({ pageTitle }: ContactScreenProps) {
   return (
-    <div className='d-flex justify-content-center'>
-      <h1 className="mt-4 aj-text-red">{ pageTitle }</h1>
+    <div className="container d-flex justify-content-center">
+      <div className='col-6 p-3'>
+        <div>
+         <h1 className="mt-4 aj-text-red text-center">{ pageTitle }</h1>
+        </div>
+        <div>
+          <form>
+            <div className="form-group mb-3">
+              <input type="text" className="form-control" id="exampleInputName" placeholder="Nome" />
+            </div>
+            <div className="form-group mb-3">
+              <input type="email" className="form-control" id="exampleInputEmail1" placeholder="E-mail" />
+            </div>
+            <div className="form-group mb-3">
+              <input type="text" className="form-control" id="exampleInputSubject" placeholder="Assunto" />
+            </div>
+            <div className="form-group mb-3">
+              <textarea className="form-control" id="exampleFormControlTextarea1" rows={5} placeholder="Mensagem"></textarea>
+            </div>
+            <button type="submit" className="btn aj-btn-red">Submit</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
