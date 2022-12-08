6c0b85058b942cdb5a73c6ec8177a1ef6401f146
douglassilvadeveloper <douglassilvadeveloper@gmail.com>
2022-12-08 18:39:59 -0300

    refactor: add key loop home

--- src/app/presentation/pages/home/index.tsx
+++ src/app/presentation/pages/home/index.tsx
@@ -9,7 +9,7 @@ export function Home() {
     <Fragment>
       <SectionHero />
       {
-        articles.map(article => <Article image={article.image} title={article.title} description={article.description} authors={article.authors} publication={article.publication}/>)
+        articles.map(article => <Article key={article.title} image={article.image} title={article.title} description={article.description} authors={article.authors} publication={article.publication}/>)
       }
     </Fragment>
   )
